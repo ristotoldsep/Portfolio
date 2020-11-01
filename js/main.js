788a0803b760e028da3f7b45cba40291c7c83395
Risto Tõldsep <r.toldsep@gmail.com>
2020-11-01 20:27:03 +0200

    Changed things for SEO

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+//MENU JS
+
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu() {
+    const menuToggle = document.querySelector(".toggle");
+    const menu = document.querySelector(".menu");
+    menuToggle.classList.toggle("active");
+    menu.classList.toggle("active");
+}
+
 // TEST TYPE EFFECT
 // ===========================
 const typedTextSpan = document.querySelector(".typed-text");
